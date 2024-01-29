68ac07616766ec4a7564b2848c2e4237587224d0
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-29 09:04:34 +0530

    Add files via upload

---
+++ numberpattrn.java
@@ -0,0 +1,14 @@
+package starpattrn;
+
+public class numberpattrn {
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 1 ; i<= 5; i++) {
+			count++;
+			for (int j =1 ; j<= i  ;j++) {
+				System.out.print(count);
+			}
+			System.out.println(" ");
+		}
+	}
+}
